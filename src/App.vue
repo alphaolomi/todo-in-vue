bd73ac07bf449105e03c58b57ca1327613b55320
Alpha Olomi <alphaolomi@gmail.com>
2019-05-03 09:18:53 +0300

    add tailwaind

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Index</router-link>
-      <router-link to="/home">Home</router-link>
-      <router-link to="/projects">Projects</router-link>
-      <router-link to="/register">Register</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/project/1">Project</router-link>
-      <router-link to="/redirect">Redirect</router-link>
-      <router-link to="/projects/create">Add</router-link>
+  <div id="app" class="mx-auto container mt-6 self-center ">
+    <nav
+      class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full"
+    >
+      <div class="mb-2 sm:mb-0 flex flex-row">
+        <div class="h-10 w-10 self-center mr-2">
+          <img class="h-10 w-10 self-center" src="https://csscomps.com/images/csscomps.png">
+        </div>
+        <div>
+          <a
+            href="/home"
+            class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold"
+          >Health Institute Projects</a>
+          <br>
+          <span class="text-xs text-grey-dark">We heal you family</span>
+        </div>
+      </div>
+
+      <div class="sm:mb-0 self-center">
+        
+        <a href="#" class="text-md no-underline text-black hover:text-blue-dark ml-2 px-1">Link1</a>
+        <a
+          href="#"
+          class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
+        >Link2</a>
+        <!-- <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About Us</a> -->
+        <a
+          href="#"
+          class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
+        >Link3</a>
+        
+      </div>
     </nav>
+
     <router-view/>
   </div>
 </template>
 
 <script>
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/tailwindcss/dist/tailwind.css";
 
 export default {
   name: "app"
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,35 +1,86 @@
 <template>
-  <div class="container">
-    <h1>Projects</h1>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>TITLE</th>
-          <th>COMPLETED</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="project in projects" :key="project.id">
-          <td>{{ project.id }}</td>
-          <td>{{ project.title }}</td>
-          <td>{{ project.completed }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class>
+    <div class="mx-auto">
+      <div class="bg-white shadow-md rounded my-12">
+        <table class="text-left w-full border-collapse">
+          <thead>
+            <tr>
+              <th
+                class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light"
+              >Id</th>
+              <th
+                class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light"
+              >title</th>
+              <th
+                class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light"
+              >Completed</th>
+              <th
+                class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light"
+              >Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover:bg-grey-lighter" v-for="project in projects" :key="project.id">
+              <td class="py-4 px-6 border-b border-grey-light">{{ project.id }}</td>
+              <td class="py-4 px-6 border-b border-grey-light">{{ project.title }}</td>
+              <td class="py-4 px-6 border-b border-grey-light">{{ project.completed }}</td>
+              <td class="border-b border-grey-light">
+                <a
+                  href="#"
+                  class="text-grey-lighter no-underline font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark"
+                >Edit</a>
+                <a
+                  href="#"
+                  class="text-grey-lighter no-underline font-bold py-1 px-3 mx-1 rounded text-xs bg-blue hover:bg-blue-dark"
+                >View</a>
+                <a
+                  href="#"
+                  class="text-grey-lighter no-underline font-bold py-1 px-3  rounded text-xs bg-red hover:bg-red-dark"
+                >Delete</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+     <div class="w-1/3 mx-auto">
+        <ul class="flex list-reset border border-grey-light rounded w-auto font-sans mx-auto">
+        <li>
+          <a
+            class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
+            href="#"
+          >Previous</a>
+        </li>
+        <li>
+          <a
+            class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
+            href="#"
+          >1</a>
+        </li>
+        <li>
+          <a
+            class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
+            href="#"
+          >2</a>
+        </li>
+        <li>
+          <a class="block text-white bg-blue border-r border-blue px-3 py-2" href="#">3</a>
+        </li>
+        <li>
+          <a class="block hover:text-white hover:bg-blue text-blue px-3 py-2" href="#">Next</a>
+        </li>
+      </ul>
+     </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
-  name: 'Projects',
-  mounted () {
-    this.$store.dispatch('FETCH_PROJECTS')
+  name: "Projects",
+  mounted() {
+    this.$store.dispatch("FETCH_PROJECTS");
   },
-  computed: mapState([
-    'projects'
-  ])
-}
-
+  computed: mapState(["projects"])
+};
 </script>
