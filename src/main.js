adc209840da21a7336fe2c474baf2eafea8ab130
Alpha Olomi <alphaolomi@gmail.com>
2019-08-02 04:51:18 +0300

    rewrite

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
-import store from './store';
-import routes from './routes';
+import router from './router'
 
 Vue.config.productionTip = false
 
-
-Vue.use(VueRouter)
-const router = new VueRouter({routes});
-
 new Vue({
-  store,
   router,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
